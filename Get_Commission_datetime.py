270803a3be22fbcb18ec8459aacccdf1245d89b9
LjaljevicStefan <97342781+LjaljevicStefan@users.noreply.github.com>
2022-04-03 19:15:27 +0200

    Add files via upload

---
+++ Get_Commission_datetime.py
@@ -0,0 +1,33 @@
+import datetime
+
+def get_commission(date_str = ''):
+    '''Function for evaluating commission based on month in year
+    
+    Parameters
+    ---------
+    date_str : str
+        String containing date in the format YYYY-MM-DD. Default is "".
+        
+    Returns
+    -------
+    Float, 0.1 is for 10% and 0.15 is for 15%. -1 is for invalid date.'''
+    
+    try:
+        date_str = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+        check = 1
+    except ValueError:
+        check = -1
+
+    if check == 1:
+        if date_str.month > 5 and date_str.month < 10:
+            return 0.15
+        else:
+            return 0.1
+    else:
+        return check
+
+if __name__ == '__main__':
+
+    date_str = input("Please enter a date in the format 'YYYY-MM-DD': ")
+
+    get_commission(date_str)
